eb3aad2c60dfeadd89b2228550424a966cfbc1e5
Yehor Komarov <ekomarov@constructor.tech>
2025-09-16 19:34:54 +0400

    moved to src

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from src.utils.add import add
+
+
+def main() -> None:
+    # variables, conditionals, while, for loops, functions
+    # classes, imports, lists, tuples, dictionaries
+    # __main__ stuff
+    print(add(5, 10))
+
+
+if __name__ == "__main__":
+    main()
