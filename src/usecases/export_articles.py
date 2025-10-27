520afca70c8eda405b3b99d5b5414d9b36759ca5
Yehor Komarov <ekomarov@constructor.tech>
2025-10-27 15:14:39 +0400

    update mongo documents

--- src/usecases/export_articles.py
+++ src/usecases/export_articles.py
@@ -3,6 +3,7 @@ from models.relational import ScientificArticle
 from models.mongo import ScientificArticle as MongoArticle, Author as MongoAuthor
 from storage.relational_db import Session
 import storage.mongo  # noqa: F401
+from mongoengine import DoesNotExist
 
 
 def export_from_db() -> None:
@@ -15,16 +16,28 @@ def export_from_db() -> None:
                 full_name=article.author.full_name,
                 title=article.author.title,
             )
-            m_article = MongoArticle(
-                db_id=article.id,
-                title=article.title,
-                summary=article.summary,
-                file_path=article.file_path,
-                created_at=article.created_at,
-                arxiv_id=article.arxiv_id,
-                author=m_author,
-            )
-            m_article.save()
+            try:
+                m_article = MongoArticle.objects.get(arxiv_id=article.arxiv_id)
+                m_article.update(
+                    db_id=article.id,
+                    title=article.title,
+                    summary=article.summary,
+                    file_path=article.file_path,
+                    created_at=article.created_at,
+                    arxiv_id=article.arxiv_id,
+                    author=m_author,
+                )
+            except DoesNotExist:
+                m_article = MongoArticle(
+                    db_id=article.id,
+                    title=article.title,
+                    summary=article.summary,
+                    file_path=article.file_path,
+                    created_at=article.created_at,
+                    arxiv_id=article.arxiv_id,
+                    author=m_author,
+                )
+                m_article.save()
 
 
 if __name__ == "__main__":
